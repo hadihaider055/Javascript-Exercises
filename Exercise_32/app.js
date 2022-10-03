0ec3053b419f5033368deeaa45da15019b8ce3ba
hadihaider055 <haiderhadi055@gmail.com>
2022-10-04 01:26:07 +0500

    fix: case-checking in ex-32

--- Exercise_32/app.js
+++ Exercise_32/app.js
@@ -5,7 +5,11 @@ let current_users = ["eric1", "john", "alice3", "mir32", "hasan32"];
 let new_users = ["eric132", "john", "alice331", "mir", "hasan32"];
 
 new_users.forEach((new_user) => {
-  if (current_users.includes(new_user)) {
+  const is_user = current_users.find((current_user) => {
+    return current_user.toLowerCase() === new_user.toLowerCase();
+  });
+
+  if (is_user) {
     console.log("You will need to enter a new username.");
   } else {
     console.log("That username is available.");
